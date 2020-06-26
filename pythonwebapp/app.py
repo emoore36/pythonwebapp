67bf510ca0bee33379c54b70eeb2d2baf3788242
emoore36 <emoore36@my.gcu.edu>
2020-06-26 08:40:51 -0700

    landing page commit

--- pythonwebapp/app.py
+++ pythonwebapp/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
+from pythonwebapp.views.index import print as index_print
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world!"
+app.register_blueprint(index_print)
